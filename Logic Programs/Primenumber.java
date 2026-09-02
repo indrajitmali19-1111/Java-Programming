12ba6705de551d3452d00b179ce8c8f74e3de77e
indrajitmali19-1111 <indrajitmali19@gmail.com>
2026-09-02 23:39:51 +0530

    logic

---
+++ Logic Programs/Primenumber.java
@@ -0,0 +1,19 @@
+//1-10 prime number find
+import java.util.*;
+
+class Primenumber{
+	public static void main(String args[]){
+	
+	int prime=2;
+	int n=10;
+	
+	
+	for(int i=prime;i<=n;i++){
+		
+		
+		if(prime%2!=0)
+		System.out.println(i);
+		prime++;
+		}
+	}
+}
