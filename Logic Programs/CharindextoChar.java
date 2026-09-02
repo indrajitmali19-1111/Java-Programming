b0bc3748ed0b2a09eafe732eb87818b7ff4760ec
indrajitmali19-1111 <indrajitmali19@gmail.com>
2026-09-02 23:15:31 +0530

    changes

--- Logic Programs/CharindextoChar.java
+++ Logic Programs/CharindextoChar.java
@@ -10,8 +10,9 @@ class CharindextoChar{
 		int index=2;
 		char character;
 		for(int i=0;i<strarr.length;i++){
-		if(i==index){
+		if(index==i){
 			character=strarr[i];
+			break;
 		}
 		}
 		System.out.println("The Character is -" + character);
