cdd1c165f0c5e9281e4d185794c86cf3e519885b
indrajitmali19-1111 <indrajitmali19@gmail.com>
2026-09-03 13:09:22 +0530

    Exception handelling

---
+++ Exception Handelling/Exceptionhandle.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Exceptionhandle{
+	public static void main(String args[]){
+		int a=11,b=0,c;
+		try{
+		c=a/b;
+		}
+		catch(Exception i){
+		System.out.println("The exception occures exception is Arithmatic exception");
+		}
+		System.out.println("The Division is");
+	}
+}
