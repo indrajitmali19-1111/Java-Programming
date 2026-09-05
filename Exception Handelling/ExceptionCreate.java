d9cd347cc4f40e6b18f761a4f6598199fb68190e
indrajitmali19-1111 <indrajitmali19@gmail.com>
2026-09-05 23:12:27 +0530

    My own Exception Create

---
+++ Exception Handelling/ExceptionCreate.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class ExceptionCreate
+{
+	public static void main(String args[])
+	{
+		int balance=5000;
+		int Withdrawl=10000;
+		
+		if(balance>Withdrawl)
+		{
+			System.out.println("Withdrawl THE Amount");
+			
+		}
+		else
+		{
+			throw new BalanceKamiAahe("Balance Kami Aahe Dada Tuja");
+		}
+	}
+}
+
+class BalanceKamiAahe extends RuntimeException
+{
+	BalanceKamiAahe(String Exception)
+	{
+		super();
+	}
+}
