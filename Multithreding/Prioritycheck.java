cac5a8e21ff38884f39e4dcba2b5a8cf3314d672
indrajitmali19-1111 <indrajitmali19@gmail.com>
2026-09-10 23:55:39 +0530

    Priority Thread Check main MEthod

---
+++ Multithreding/Prioritycheck.java
@@ -0,0 +1,11 @@
+//Main meethod Default priority check
+
+import java.util.*;
+
+class Prioritycheck
+{
+	public static void  main(String args[])
+	{
+		System.out.println(Thread.currentThread().getPriority());
+	}
+}
